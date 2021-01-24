2666ddba927b6903cf1e7c93333e0f7f486e8ea9
Mattias Högnäs <mattias.hognas@gmail.com>
2021-01-24 04:49:35 +0000

    Added devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,5 @@
+git config --global user.email "mattias.hognas@gmail.com"
+git config --global user.name "Mattias Högnäs"
+
+deno install -qAf --unstable https://deno.land/x/denon@2.4.6/denon.ts
+export PATH="/root/.deno/bin:$PATH"
