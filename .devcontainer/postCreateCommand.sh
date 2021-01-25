e28b43fb8b7f48940f0df8dc950a257a29b95d80
Mattias Högnäs <mattias.hognas@gmail.com>
2021-01-25 00:41:18 +0000

    Container cleanup

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,4 @@
 git config --global user.email "mattias.hognas@gmail.com"
 git config --global user.name "Mattias Högnäs"
 
-deno install -qAf --unstable https://deno.land/x/denon@2.4.6/denon.ts
-export PATH="/root/.deno/bin:$PATH"
+deno install -A -f -n aleph https://deno.land/x/aleph@v0.2.28/cli.ts
