fa81c22fc4ca89c0c8d8a81e7b84542868702618
Mattias Högnäs <mattias.hognas@gmail.com>
2021-01-26 21:49:11 +0000

    Fixed dev container

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,3 +2,4 @@ git config --global user.email "mattias.hognas@gmail.com"
 git config --global user.name "Mattias Högnäs"
 
 deno install -A -f -n aleph https://deno.land/x/aleph@v0.2.28/cli.ts
+export PATH="/root/.deno/bin:$PATH"
